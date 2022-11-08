3bbf783b578cdcc69d805e96290eff8d71ea33f8
msucil <np.msushil@gmail.com>
2022-11-08 21:10:19 +0800

    Initalize project

---
+++ src/main/java/com/msucil/app/lomba/LombaApplication.java
@@ -0,0 +1,13 @@
+package com.msucil.app.lomba;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LombaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LombaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/msucil/app/lomba/LombaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.msucil.app.lomba;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LombaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
